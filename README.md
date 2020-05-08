54a20f72987b56b6284d9a3f84e1975f0799ba38
porsmbao <bao.xinwei1995@gmail.com>
2020-05-08 20:41:20 +0800

    final version

--- README.md
+++ README.md
@@ -1,28 +1,24 @@
----
-title: "README"
-output: html_document
----
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
 
-## R Markdown
+## CodeBook.md
 
-This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
+This file includes an explanation of the codes used to generate tidy data for analysis.
 
-When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
 
-```{r cars}
-summary(cars)
-```
 
-## Including Plots
 
-You can also embed plots, for example:
+## tidy_data.txt
 
-```{r pressure, echo=FALSE}
-plot(pressure)
-```
+This file is the tidy data generated from instruction 1 to 4.
 
-Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
+
+
+
+
+## average_data.txt
+
+This file is the new average data of all measurements for each subject and activity generated from instruction 5.
+
+## run_analysis.R
+
+This file is the R script that clean and tidy up the data in order to get ready for further analysis.
---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+library(data.table)
+library(dplyr)
+
+#reading data
+features <- fread("./data/features.txt", sep = " ", stringsAsFactors = F, header = F)
+test_df <- fread("./data/test/X_test.txt", sep = " ", stringsAsFactors = F, header = F)
+train_df <- fread("./data/train/X_train.txt", sep = " ", stringsAsFactors = F, header = F)
+test_activity <- readLines("./data/test/y_test.txt")
+train_activity <- readLines("./data/train/y_train.txt")
+subject_test <- fread("./data/test/subject_test.txt", stringsAsFactors = F, header = F)
+subject_train <- fread("./data/train/subject_train.txt", stringsAsFactors = F, header = F)
+#add activity column to data
+test_df <- mutate(test_df, activity = test_activity)
+test_df <- mutate(test_df, subject = subject_test$V1)
+train_df <- mutate(train_df, activity = train_activity)
+train_df <- mutate(train_df, subject = subject_train$V1)
+
+#merging 2 sets
+dt <- add_row(test_df,train_df)
+
+#subsetting those with "mean" and "std" inside
+dt <- select(dt, grep("mean|std", features$V2), activity, subject)
+
+#replace numbers with descriptive values
+dt$activity <- gsub("1", "WALKING", dt$activity)
+dt$activity <- gsub("2", "WALKING_UPSTAIRS", dt$activity)
+dt$activity <- gsub("3", "WALKING_DOWNSTAIRS", dt$activity)
+dt$activity <- gsub("4", "SITTING", dt$activity)
+dt$activity <- gsub("5", "STANDING", dt$activity)
+dt$activity <- gsub("6", "LAYING", dt$activity)
+
+#use descriptive column names
+cnames <- unlist(unclass(features[grep("mean|std", features$V2),2])$V2)
+colnames(dt) <- c(cnames, "activity", "subject")
+
+#creating new dataset with average values of measurements by each subject and activity
+avg <- dt %>% 
+        group_by(subject) %>% 
+        group_by(activity) %>% 
+        mutate_at(1:79,mean) %>% 
+        distinct()%>%
+        select(subject, activity, 1:79) %>%
+        arrange(subject, activity)
+
+write.table(avg, file = "average_data.txt", row.name = F)
+write.table(dt, file = "tidy_data.txt", row.name = F)
